a61e046ad9318da21675f5d84b1efb3718344e09
Animalovsky <maugustyn.webdesign@gmail.com>
2019-02-25 23:49:44 +0000

    Second commit.

---
+++ js/quiz.js
@@ -0,0 +1,97 @@
+$('#quiz').quiz({
+	//resultsScreen: '#results-screen',
+	//counter: false,
+	//homeButton: '#custom-home',
+	counterFormat: 'Question %current of %total',
+	questions: [{
+		'q': '1. How many bones does the face contain?',
+		'options': ['1. 40', '2. 12', '3. 16', '4. 22'],
+		'correctIndex': 3,
+		'correctResponse': 'Well done!',
+		'incorrectResponse': 'The human head contains 22 bones, consisting the cranium and the facial bones. The cranium is formed by 8 bones: the frontal bone, two parietal bones, two temporal bones, the occipital bone in the back, the ethmoid bone behind the nose, and the sphenoid bone.'
+	}, {
+		'q': '2. Is female brain mass smaller than that of male?',
+		'options': ['True', 'False'],
+		'correctIndex': 0,
+		'correctResponse': 'Correct!',
+		'incorrectResponse': 'Female average brain mass is slightly less than that of males.'
+	}, {
+		'q': '3. Our bodies cannot make the minerals they require (such as iron, calcium, copper and zinc), so all of our minerals are obtained from our food and drink.',
+		'options': ['True', 'False'],
+		'correctIndex': 0,
+		'correctResponse': 'Good Job!',
+		'incorrectResponse': 'Minerals play an important part in keeping our bodies working properly, such as maintaining our heart rhythm, forming our teeth and bones, and helping to regulate cellular metabolism.'
+	}, {
+		'q': '4. What percentage of the human body is water?',
+		'options': ['1. 50%', '2. 60%', '3. 75%', '4. 80%'],
+		'correctIndex': 1,
+		'correctResponse': 'Thats right!',
+		'incorrectResponse': 'On average, the body of an adult human being contains 60% water. Most of the water in the human body is contained inside our cells.'
+	}, {
+		'q': '5. Which human organ serves human beings with ample physical and cognitive abilites?',
+		'options': ['1. Liver', '2. Lungs', '3. Heart', '4. Brain'],
+		'correctIndex': 3,
+		'correctResponse': 'Correct!',
+		'incorrectResponse': 'A command center for the central nervous system, the brain serves human beings with ample physical and cognitive abilities. '
+	}, {
+		'q': '6. A synonim for human chest is called thorax.',
+		'options': ['True', 'False'],
+		'correctIndex': 0,
+		'correctResponse': 'Well done!',
+		'incorrectResponse': 'The human chest (from the Greek thorax "breastplate, cuirass, corslet", via Latin: thorax) is a part of the anatomy of humans and various other animals located between the neck and the abdomen.'
+	}, {
+		'q': '7. How many bones does the human skeleton consists of?',
+		'options': ['1. 185', '2. 225', '3. 206', '4. 270'],
+		'correctIndex': 2,
+		'correctResponse': 'Correct!',
+		'incorrectResponse': 'The skeleton of an adult human consists of 206 bones. It is composed of 270 bones at birth, which decreases to 206 bones by adulthood after some bones have fused together.'
+	}, {
+		'q': '8. How many chambers does a normal human heart have ?',
+		'options': ['1', '2', '4', '6'],
+		'correctIndex': 2,
+		'correctResponse': 'Good Job!',
+		'incorrectResponse': 'The human heart has four chambers, which are the left and right atria and the left and right ventricles. Blood enters the right atrium where it then passes through the right ventricle.'
+	}, {
+		'q': '9. The area exposed by open-necked shirts, the V of the chest is sometimes the location of a light-induced skin disease called?',
+		'options': ['1. Hyperpigmentation', '2. Sunburn', '3. Asthma', '4. Polymorphous light eruption'],
+		'correctIndex': 3,
+		'correctResponse': 'Thats right!',
+		'incorrectResponse': 'Polymorphic light eruption is a fairly common skin rash triggered by exposure to sunlight or artificial ultraviolet (UV) light.'
+	}, {
+		'q': '10. Which three layers make up our skin?',
+		'options': ['1. Epidermis, dermis and hypodermis', '2. Pigment, dermis and hypodermis', '3. Epidermis, dermis and muscles', '4. Hair, epidermis and blood vessels'],
+		'correctIndex': 0,
+		'correctResponse': 'Correct!',
+		'incorrectResponse': 'The epidermis, the outermost layer of skin, provides a waterproof barrier and creates our skin tone. The dermis, beneath the epidermis, contains tough connective tissue, hair follicles, and sweat glands. The deeper subcutaneous tissue (hypodermis) is made of fat and connective tissue.'
+	}, {
+		'q': '11. The arm is made up of 4 long bones.',
+		'options': ['True', 'False', ],
+		'correctIndex': 1,
+		'correctResponse': 'Well done!',
+		'incorrectResponse': 'Your arm is made up of three long bones: your humerus in your upper arm and your ulna and radius in your lower arm.'
+	}, {
+		'q': '12. Considering the human trunk is 100%, by how many percentages are human legs longer?',
+		'options': ['1. None', '2. 125%', '3. 171%', '4. 158%'],
+		'correctIndex': 2,
+		'correctResponse': 'Thats right!',
+		'incorrectResponse': 'The human legs are exceptionally long and powerful as a result of their exclusive specialization to support and locomotion — in orangutans the leg length is 111% of the trunk; in chimpanzees 128%, and in humans 171%.'
+	}, {
+		'q': '13. Muscle action can be classified as being either voluntary or involuntary.',
+		'options': ['True', 'False'],
+		'correctIndex': 0,
+		'correctResponse': 'Good Job!',
+		'incorrectResponse': 'Cardiac and smooth muscles contract without conscious thought and are termed involuntary, whereas the skeletal muscles contract upon command. Skeletal muscles in turn can be divided into fast and slow twitch fibers.'
+	}, {
+		'q': '14. What is the human body’s biggest organ?',
+		'options': ['1. Brain', '2. Skin', '3. Lungs', '4. Stomach'],
+		'correctIndex': 1,
+		'correctResponse': 'Thats right!',
+		'incorrectResponse': 'The human skin is the outer covering of the body. In humans, it is the largest organ of the integumentary system. The skin has up to seven layers of ectodermal tissue and guards the underlying muscles, bones, ligaments and internal organs.'
+	}, {
+		'q': '15. How many lungs does the human body have?',
+		'options': ['1. 3', '2. 2', '3. 0', '4. 1'],
+		'correctIndex': 1,
+		'correctResponse': 'Correct!',
+		'incorrectResponse': 'Humans have two lungs, a right lung and a left lung. They are situated within the thoracic cavity of the chest. The right lung is bigger than the left, which shares space in the chest with the heart.'
+	}]
+});
